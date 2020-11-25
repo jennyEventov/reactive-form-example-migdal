f3ab6cdbf18daa49a86b141e30620ccbf0fd5f90
jennyEventov <50540032+jennyEventov@users.noreply.github.com>
2020-11-25 17:25:13 +0200

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,8 +3,7 @@ import { ContactPerson} from './components/reactive-form-contact-persons/contact
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   isShown = false;
